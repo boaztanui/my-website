75ea122f915073a7eb152a9282caf61287779d04
Boaz <boaztanui058@gmail.com>
2021-07-05 11:26:21 +0300

    corrected mistakes on Project container component

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -23,7 +23,7 @@ function ProjectContainer() {
                     liveLink='https://netflix-clone-8d40d.web.app/'
                     img={NetflixClone}
                     header='Netflix Clone'
-                    content='This project was all about fetching data from an API using React Hooks and displaying the movies in a UI same as Netflix.  I used Styled components for styling, utilised material UI for icons, React-router and delopyed it to production on Firebase'
+                    content='This project was all about fetching data from an API using React Hooks and displaying the movies in a UI same as Netflix.  I used Styled components for styling, utilised material UI for icons, React-router and deployed it to production on Firebase'
                     fullStack={['React', 'React-Router', 'Material-UI', 'Firebase',]}
                 />
                 <Projects
@@ -31,7 +31,7 @@ function ProjectContainer() {
                     liveLink='http://boaztanui.com/'
                     img={Portfolio}
                     header='Portfolio Website'
-                    content='I created a React app to showcase my work and skills.  I used React Components to divide each section into single components. I used Syled components for tyling this App. I also delpoyed it on firebase'
+                    content='I created a React app to showcase my work and skills.  I used React Components to divide each section into single components. I used Styled components for styling this App. I also deployed it on firebase'
                     fullStack={['React', 'Styled Components', 'React-Router']}
                 />
                 <Projects
@@ -39,7 +39,7 @@ function ProjectContainer() {
                     liveLink='https://tinder-clone-4ca20.web.app/'
                     img={TinderClone}
                     header='Tinder Clone'
-                    content='This projects was build with a purpose of connecting the React frontEnd with Node Backend. I used MongoDB, Express and Node.js for the backend and storing of Data. The App had most functionalities of Tinder App'
+                    content='This project was built with a purpose of connecting the React frontend with Node Backend. I used MongoDB, Express and Node.js for the backend and storing of Data. The App had most functionalities of Tinder App'
                     fullStack={['React', 'NODE.JS', 'Express', 'React-Router']}
                 />
 
@@ -49,7 +49,7 @@ function ProjectContainer() {
                     liveLink='https://slack-clone-992e5.web.app/'
                     img={SlackClone}
                     header='Slack Clone'
-                    content='I created Slack clone because I liked the simplicity of their UI. Using Material UI for Icons was Amazing. Used google Auth for logIn. It has an ability for anyone to create a Group just like Slack and also comment on other groups'
+                    content='I created Slack clone because I liked the simplicity of their UI. Using Material UI for Icons was Amazing. Used google Auth for login. It has an ability for anyone to create a Group just like Slack and also comment on other groups'
                     fullStack={['Redux', 'React', 'Firebase', 'Styled Components']}
                 />
                 <Projects
@@ -57,7 +57,7 @@ function ProjectContainer() {
                     liveLink='https://tesla-clone-676a7.web.app/'
                     img={TeslaClone}
                     header='Tesla Clone'
-                    content='This project was about Designing the framework of Tesla using React. It was so amazing to use diffent animations in various sections of the APP I used react Hooks, Css3, Styled Components and delopyed it to production on Firebase'
+                    content='This project was about Designing the framework of Tesla using React. It was so amazing to use different animations in various sections of the APP I used react Hooks, Css3, Styled Components and deployed it to production on Firebase'
                     fullStack={['React', 'Firebase', 'css3', 'React-Router']}
                 />
                 <Projects
@@ -65,7 +65,7 @@ function ProjectContainer() {
                     liveLink='https://github.com/boaztanui/crwn-clothing'
                     img={CrownClothing}
                     header='Crown Clothing App'
-                    content='This was one of my largest build i created on an online bootcamp. The Goal was to design an Ecommerce website with all the functionalities working including making payments, login with google and delopyed it to production on Firebase '
+                    content='This was one of my largest build I created on an online bootcamp. The Goal was to design an Ecommerce website with all the functionalities working including making payments, login with google and deployed it to production on Firebase'
                     fullStack={['React', 'Firebase', 'Scss', 'GraphQL']}
                 />
             </ProjectBox>
